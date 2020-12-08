6428f0907f61970f932f3664e6503ea009d88f61
Jesper Eriksen <jespertweriksen@gmail.com>
2020-12-08 19:17:44 +0100

    home/index changes

--- WebService/wwwroot/js/viewModel.js
+++ WebService/wwwroot/js/viewModel.js
@@ -1,20 +1,24 @@
 ﻿define(['knockout', 'postman'], (ko, postman) => {
     let currentComponent = ko.observable("home");
+    let bodyComponent = ko.observable("home");
     let menuElements = ["Home", "Movie", "Persons", "Create User", "Profile", "About"];
+
     let changeContent = element => {
-        
         currentComponent(element.toLowerCase());
+        bodyComponent = currentComponent;
     }
 
     let isActive = element => {
         return element.toLowerCase() === currentComponent() ? "active" : "";
     }
 
+
     postman.subscribe("changeContent", component => {
         changeContent(component);
     });
     return {
         currentComponent,
+        bodyComponent,
         menuElements,
         changeContent,
         isActive
