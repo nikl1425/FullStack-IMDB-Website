856f8f245434d2932d1aeebaac2423f899d7aedb
Jesper Eriksen <jespertweriksen@gmail.com>
2020-12-09 23:48:21 +0100

    top poster fetch

--- WebService/wwwroot/js/components/home/home.js
+++ WebService/wwwroot/js/components/home/home.js
@@ -5,6 +5,21 @@ define(['postman'], (postman) => {
             postman.publish("changeContent", "movie");
         }
 
+        var jsonObj;
+
+        fetch('http://localhost:5001/api/title/topposter')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const myObjStr = JSON.stringify(eval(data));
+                jsonObj = JSON.parse(myObjStr);
+                console.log(jsonObj);
+                return jsonObj;
+            })
+            .then(data => obj = data);
+
+
         return {
             gotoMovie
         };
